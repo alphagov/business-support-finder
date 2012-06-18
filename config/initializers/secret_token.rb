765258023a66b5dc4f9c362caa9c537a290f5443
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-06-18 10:17:02 +0100

    Start new rails 3.2.3 project
    rails new finance-finder -O -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinanceFinder::Application.config.secret_token = '9cdc364e9086579dedaf6c28e3c48de3043648fc337b24d68cd204990f4c543cd37d8a41904457bedbaeacfbad16fd444bc62120b0cdcfe1c563b29cc7a6098e'
