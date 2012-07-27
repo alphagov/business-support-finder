7a79eacb5720fc8a17d9a05894751665d37a1517
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-07-27 10:00:34 +0100

    Added conroller specs

--- spec/controllers/finance_finder_controller_spec.rb
+++ spec/controllers/finance_finder_controller_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe FinanceFinderController do
 
+  describe "GET to index" do
+    before :each do
+      5.times { |idx| FactoryGirl.create(:business_support, title: "Example Business Support #{idx}") }
+      get 'index'
+    end
+    it "returns HTTP success" do
+      response.should be_success
+    end
+    it "finds business support records" do
+      assert assigns[:finance_supports].size == 5
+    end
+  end
+
 end
