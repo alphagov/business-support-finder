3785b72f066762b72b3c7bf8c8ae0bd6d7e22d96
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-09-20 15:16:12 +0100

    Add initial start page.

---
+++ app/controllers/business_support_controller.rb
@@ -0,0 +1,13 @@
+class BusinessSupportController < ApplicationController
+
+  before_filter :set_slimer_headers
+
+  def start
+  end
+
+  private
+
+  def set_slimer_headers
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BusinessSupportFinder::Application.routes.draw do
 
+  get "/#{APP_SLUG}" => "business_support#start"
+
   root :to => redirect("/#{APP_SLUG}", :status => 302)
 end
