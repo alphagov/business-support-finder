9ba7e2ab29d28a988a1cd03b02b485f20ca86ae5
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-09-21 13:05:19 +0000

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ BusinessSupportFinder::Application.configure do
   # config.threadsafe!
 
   # Enable lograge
-  config.lgorage.enabled = true
+  config.lograge.enabled = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
