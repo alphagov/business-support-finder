d3c0a1b5b066f667181917a9a11ff35442fdd282
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-09-26 16:16:00 +0000

    Add 'Change this answer' links.

--- spec/integration/support_options_page_spec.rb
+++ spec/integration/support_options_page_spec.rb
@@ -16,4 +16,21 @@ describe "Support options page" do
 
     page.should have_content("Available support")
   end
+
+  specify "inspecting the 'change this answer' links" do
+    visit "/#{APP_SLUG}/support-options?sectors=health_manufacturing&stage=start-up&structure=partnership&location=england"
+
+    within_section "completed question 1" do
+      page.should have_link("Change this answer", :href => "/#{APP_SLUG}/sectors?location=england&sectors=health_manufacturing&stage=start-up&structure=partnership")
+    end
+    within_section "completed question 2" do
+      page.should have_link("Change this answer", :href => "/#{APP_SLUG}/stage?location=england&sectors=health_manufacturing&stage=start-up&structure=partnership")
+    end
+    within_section "completed question 3" do
+      page.should have_link("Change this answer", :href => "/#{APP_SLUG}/structure?location=england&sectors=health_manufacturing&stage=start-up&structure=partnership")
+    end
+    within_section "completed question 4" do
+      page.should have_link("Change this answer", :href => "/#{APP_SLUG}/location?location=england&sectors=health_manufacturing&stage=start-up&structure=partnership")
+    end
+  end
 end
