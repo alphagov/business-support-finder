d18308a8223a433c58f7725210c35599bbe6b8d4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-10-01 15:58:56 +0100

    Add Scheme model to abstract the calls to APIs

---
+++ app/models/scheme.rb
@@ -0,0 +1,32 @@
+require 'ostruct'
+require 'gds_api/helpers'
+
+class Scheme < OpenStruct
+  extend GdsApi::Helpers
+
+  def self.lookup(facets)
+    possible_schemes = imminence_api.business_support_schemes(
+      :sectors => facets[:sectors].map(&:slug).join(','),
+      :stages => facets[:stage].slug,
+      :business_types => facets[:structure].slug,
+      :locations => facets[:location].slug
+    )
+    schemes = content_api.business_support_schemes(possible_schemes.results.map {|s| s["business_support_identifier"] } )
+
+    schemes.results.map do |s|
+      self.new(s)
+    end
+  end
+
+  def initialize(artefact = {})
+    super()
+    if (details = artefact.delete("details"))
+      details.each do |k,v|
+        self.send("#{k}=", v)
+      end
+    end
+    artefact.each do |k,v|
+      self.send("#{k}=", v)
+    end
+  end
+end
