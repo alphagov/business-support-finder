cf4479d83db6ff5a5e07cf7f4ebeaec969fe7f3a
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2012-10-14 16:00:51 +0100

    Add 'type' of support scheme question to flow

---
+++ app/models/types.rb
@@ -0,0 +1,36 @@
+require 'ostruct'
+
+class Types < OpenStruct
+  HARDCODED_STAGES = {
+    "finance" => "Finance, grants and loans",
+    "all" => "Expertise, advice and finance"
+  }.map do |slug, name|
+    new(:slug => slug, :name => name)
+  end
+
+  def self.all
+    HARDCODED_STAGES
+  end
+
+  def self.find_by_slug(slug)
+    HARDCODED_STAGES.find do |stage|
+      stage.slug == slug
+    end
+  end
+  
+  def self.convert_to_types(slug)
+    
+    if slug == 'finance'
+      business_support_types = 'finance,grant,loan,equity'
+    elsif slug == 'all'
+      business_support_types = 'expertise-and-advice,recognition-award,equity,loan,finance,grant'
+    else
+      business_support_types = ''
+    end
+    business_support_types
+  end
+
+  def to_s
+    name
+  end
+end
