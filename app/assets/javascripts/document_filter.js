334398eb88c615b17c8428e4faad993dfa720187
tombye <tombaromba@gmail.com>
2013-09-04 17:45:16 +0100

    Indent HTML & fix JS structure

--- app/assets/javascripts/document_filter.js
+++ app/assets/javascripts/document_filter.js
@@ -7,12 +7,12 @@
 /*global jQuery */
 if(typeof window.GOVUK === 'undefined'){ window.GOVUK = {}; }
 if(typeof window.GOVUK.support === 'undefined'){ window.GOVUK.support = {}; }
+
+window.GOVUK.support.history = function() {
+  return window.history && window.history.pushState && window.history.replaceState;
+};
 (function($) {
   "use strict";
-
-  window.GOVUK.support.history = function() {
-    return window.history && window.history.pushState && window.history.replaceState;
-  } 
 
   var documentFilter = {
     loading: false,
