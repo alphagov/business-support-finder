e4fc3bc7f5c774f5fdb85cf129651827eff682ce
tombye <tombaromba@gmail.com>
2013-09-06 19:12:52 +0100

    Add schemes data and support for it in script

--- app/assets/javascripts/document_filter.js
+++ app/assets/javascripts/document_filter.js
@@ -18,7 +18,107 @@ window.GOVUK.support.history = function() {
     loading: false,
     $form: null,
     formType: '',
+    staticData: null,
 
+    getData: function(params) {
+      var $form = this.$form,
+          $submitButton = $form.find('input[type=submit]');
+
+      $.ajax($form.attr('action'), {
+        cache: false,
+        dataType:'json',
+        data: params,
+        complete: function(){
+          //documentFilter.loading = false;
+        },
+        success: function(data) {
+          documentFilter.staticData = data;
+        },
+        error: function() {
+          $submitButton.removeAttr('disabled');
+        }
+      });
+    },
+    filterParams: function(params) {
+      var supportFilters = [],
+          tempParams = [],
+          mappings = {
+            "business-type-option": "sectors",
+            "business-stage-option": "stages",
+            "types[]": "support_types",
+            "location": "locations"
+          };
+      $.map(params, function (param) {
+        if (param.name === 'types[]') {
+          supportFilters.push(param.value);
+        }
+        else { tempParams.push(param); }
+      });
+
+      if (supportFilters.length) { 
+        params = tempParams;
+        params.push({ 'name': 'types[]', 'value': supportFilters }); 
+        tempParams = [];
+      }
+
+      $.map(params, function (param) {
+        param.name = mappings[param.name];
+        return param;
+      });
+
+      return params;
+    },
+    sortData: function(params) {
+      var items = this.staticData,
+          itemNum = items.length,
+          results = [],
+
+          paramInItem = function(item, param) {
+            if (typeof item[param.name] !== 'undefined' && $.inArray(param.value, item[param.name])) {
+              return true;
+            }
+            return false;
+          },
+          
+          isValidItem = function(item) {
+            var paramNum = params.length,
+                paramName,
+                paramValues,
+                param,
+                valueMatches,
+                paramMatches = 0;
+
+            while (paramNum--) {
+              if ($.isArray(params[paramNum].value)) {
+                valueMatches = 0;
+                paramName = params[paramNum].name;
+                paramValues = params[paramNum].value;
+                $.map(params[paramNum].value, function (paramValue) {
+                  if (paramInItem(item, { 'name': paramName, 'value': paramValue})) { valueMatches++; }
+                });
+                if (valueMatches === paramValues.length) { paramMatches++ }
+              }
+              else {
+                param = params[paramNum];
+                if (param.value === 'all') {
+                  paramMatches++;
+                }
+                else {
+                  if (paramInItem(item, param)) { paramMatches++ }
+                }
+              }
+            }
+
+            return (paramMatches === params.length);
+          };
+
+      while (itemNum--) {
+        if (isValidItem(items[itemNum])) {
+          results.push(items[itemNum]);
+        }
+      }
+      return results;
+    },
     renderTable: function(data) {
       $('.js-filter-results').mustache('documents-_filter_table', data);
     },
@@ -43,13 +143,15 @@ window.GOVUK.support.history = function() {
           $submitButton = $form.find('input[type=submit]'),
           url = $form.attr('action'),
           jsonUrl = url + ".json",
-          params = $form.serializeArray();
+          params = $form.serializeArray(),
+          data;
 
-      $submitButton.addClass('disabled');
+      $('.submit .button').addClass('disabled');
       $(".filter-results-summary").find('.selections').text("Loading results…");
       $(".feeds").addClass('js-hidden');
-      documentFilter.loading = true;
+      //documentFilter.loading = true;
       // TODO: make a spinny updating thing
+      /*
       $.ajax(jsonUrl, {
         cache: false,
         dataType:'json',
@@ -72,6 +174,11 @@ window.GOVUK.support.history = function() {
           $submitButton.removeAttr('disabled');
         }
       });
+      */
+      params = documentFilter.filterParams(params);
+      data = documentFilter.sortData(params);
+      documentFilter.renderTable(data);
+      documentFilter.liveResultCounter(data);
     },
     urlWithout: function(object, value){
       var url = window.location.search,
@@ -143,12 +250,16 @@ window.GOVUK.support.history = function() {
     }
     this.each(function(){
       if (window.GOVUK.support.history()) {
-        var $form = $(this);
+        var $form = $(this),
+            params = $form.serializeArray();
         $(window).on('popstate', function(evet) {
           documentFilter.onPopState(event);
         });
         documentFilter.$form = $form;
         documentFilter.formType = $form.attr('action').split('/').pop();
+
+        params = documentFilter.filterParams(params);
+        documentFilter.getData(params);
 
         history.replaceState(documentFilter.currentPageState(), null);
         $form.submit(documentFilter.submitFilters);
@@ -171,9 +282,9 @@ window.GOVUK.support.history = function() {
         });
 
         $(".submit").addClass("js-hidden");
-
       }
     });
+
     return this;
   }
 
