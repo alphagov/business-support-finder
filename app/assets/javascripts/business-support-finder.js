350c54d4b7f1ea9fa936a227d360b17757994478
Tim Abell <tim@timwise.co.uk>
2013-09-26 16:31:17 +0100

    Remove javascript, moving to non-js implementation
    Wip implementing new design for this tool.
    Prototype for user testing was client-side mockup,
    real version will be progressive enhancement so starting without
    javascript.
    Javascript can be re-added later as needed.
    
    https://www.pivotaltracker.com/story/show/57312614

--- app/assets/javascripts/business-support-finder.js
+++
@@ -1,4 +0,0 @@
-// Business support finder manifest
-//= require accounting
-//= require document_filter
-//= require on_ready
--- app/assets/javascripts/on_ready.js
+++
@@ -1,4 +0,0 @@
-$(".js-document-filter").enableDocumentFilter({
-  "paramTests": { "max_employees": "max_is_less_than" },
-  "requiredParams":  ["support_types"]
-}); 
