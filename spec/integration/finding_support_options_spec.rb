fccdabfd18410dd3feb123a2b6dad3f32fa5f9bb
Tim Abell <tim@timwise.co.uk>
2013-10-08 18:11:28 +0100

    Check result element count
    Work in progress, needs capybara update

--- spec/integration/finding_support_options_spec.rb
+++ spec/integration/finding_support_options_spec.rb
@@ -8,6 +8,7 @@ describe "Finding support options" do
       [
         {"title" => "Graduate start-up", "business_support_identifier" => "graduate-start-up"},
         {"title" => "Manufacturing Services - Wales", "business_support_identifier" => "manufacturing-services-wales"},
+        {"title" => "An unfiltered scheme", "business_support_identifier" => "unfiltered-scheme"},
       ]
     )
     stub_imminence_business_support_schemes_for_filter(
@@ -47,14 +48,16 @@ describe "Finding support options" do
   it "should show all available schemes by default" do
     page.should have_content 'Graduate start-up scheme'
     page.should have_content 'Manufacturing Services scheme - Wales'
-    page.should have_selector('.filter-results-summary h3 span', text: '2') # result count
+    page.assert_selector('li.scheme')
+    page.should have_selector('.filter-results-summary h3 span', text: '3') # result count
   end
 
   it "should allow filtering" do
     select "Start-up", :from => "Business stage"
     click_on "Refresh results"
+    page.assert_selector('li.scheme', count: 1)
     page.should have_content 'Graduate start-up scheme'
-    page.should_not have_content 'Manufacturing Services scheme - Wales'
+    page.should_not have_content 'An unfiltered scheme'
     page.should have_selector('.filter-results-summary h3 span', text: '1') # result count
   end
 end
