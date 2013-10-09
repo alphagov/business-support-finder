02110b83c24084671d11f192f6928004e38a4f5d
Tim Abell <tim@timwise.co.uk>
2013-10-09 17:47:32 +0100

    Setup gds_api on features (part 2)
    Following gem upgrades

--- spec/support/gds_api.rb
+++ spec/support/gds_api.rb
@@ -3,9 +3,9 @@ require 'gds_api/test_helpers/content_api'
 require 'gds_api/test_helpers/imminence'
 
 RSpec.configure do |config|
-  config.include GdsApi::TestHelpers::ContentApi, :type => :controller
+  config.include GdsApi::TestHelpers::ContentApi, :type => :feature
   config.include GdsApi::TestHelpers::Imminence, :type => :feature
-  config.before(:each, :type => :controller) do
+  config.before(:each, :type => :feature) do
     stub_content_api_default_artefact
     stub_imminence_default_business_support_schemes
   end
