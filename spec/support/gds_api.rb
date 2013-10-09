fe2eec5e5f03e315acde5087dae0dff7be12c858
Tim Abell <tim@timwise.co.uk>
2013-10-09 17:12:10 +0100

    Include gds_api stubs in feature tests
    Consequence of gem upgrade.

--- spec/support/gds_api.rb
+++ spec/support/gds_api.rb
@@ -4,7 +4,7 @@ require 'gds_api/test_helpers/imminence'
 
 RSpec.configure do |config|
   config.include GdsApi::TestHelpers::ContentApi, :type => :controller
-  config.include GdsApi::TestHelpers::Imminence, :type => :controller
+  config.include GdsApi::TestHelpers::Imminence, :type => :feature
   config.before(:each, :type => :controller) do
     stub_content_api_default_artefact
     stub_imminence_default_business_support_schemes
