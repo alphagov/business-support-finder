11b8299cbd6d1b0b89140bf95f2fa07955da93fd
Tim Abell <tim@timwise.co.uk>
2013-10-17 15:14:35 +0100

    Handle legacy application route requests

--- spec/integration/root_redirect_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Redirecting the root URL" do
-  it "should redirect to the start page" do
-    get "/"
-
-    response.should redirect_to("/#{APP_SLUG}")
-  end
-end
