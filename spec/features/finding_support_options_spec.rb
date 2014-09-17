c7f911fb376cbc9aff629312676f419a4edef036
steventux <steve.laing@digital.cabinet-office.gov.uk>
2014-09-17 09:49:14 +0000

    Replace locations/areas with postcode field
    - Remove Location selection
    - Add Business postcode field
    - Ensure updated to postcode field don't fire JS filtering

--- spec/features/finding_support_options_spec.rb
+++ spec/features/finding_support_options_spec.rb
@@ -66,13 +66,13 @@ describe "Finding support options" do
           [
             { "title" => "Graduate start-up", "business_support_identifier" => "graduate-start-up",
               "stages" => ["grow-and-sustain"], "business_sizes" => ["up-to-249"], "sectors" => ["education"],
-              "areas" => ["london"], "support_types" => ["finance", "equity", "grant", "expertise-and-advice"] },
+              "support_types" => ["finance", "equity", "grant", "expertise-and-advice"] },
           ],
           {
                 "stages" => "grow-and-sustain",
                 "business_sizes" => "up-to-249",
                 "sectors" => "education",
-                "areas" => "london",
+                "postcode" => "WC2B 6SE",
                 "support_types" => "finance,equity,grant,expertise-and-advice",
           }
         )
@@ -82,7 +82,6 @@ describe "Finding support options" do
       it "should filter results" do
         uncheck("loan")
         uncheck("recognition-award")
-        select "London", :from => "areas"
         select "10 - 249", :from => "business_sizes"
         select "Education", :from => "sectors"
         select "Grow and sustain", :from => "stages"
@@ -91,6 +90,18 @@ describe "Finding support options" do
         page.should have_content 'Graduate start-up'
         page.should have_selector('.filter-results-summary h3 span', text: '1') # result count
       end
+
+      it "should return no results for incomplete postcodes" do
+        fill_in "Business postcode", :with => "WC2B"
+        click_on "Refresh results"
+        page.assert_selector('li.scheme', count: 0)
+      end
+
+      it "should filter results with a valid postcode" do
+        fill_in "Business postcode", :with => "WC2B 6SE"
+        click_on "Refresh results"
+        page.assert_selector('li.scheme', count: 1)
+      end
     end
   end
 
@@ -98,20 +109,18 @@ describe "Finding support options" do
     before do
       Capybara.current_driver = Capybara.javascript_driver
 
-      business_support_api_has_schemes(
-        [
-          { "title" => "Graduate start-up", "business_support_identifier" => "graduate-start-up",
-            "stages" => ["grow-and-sustain"], "business_sizes" => ["up-to-249"], "sectors" => ["education"],
-            "areas" => ["london"], "support_types" => ["finance", "equity", "grant", "expertise-and-advice"] },
-          { "title" => "Manufacturing Services - Wales", "business_support_identifier" => "manufacturing-services-wales",
-            "stages" => ["grow-and-sustain"], "business_sizes" => ["under-10"], "sectors" => ["manufacturing"],
-            "areas" => ["wales"], "support_types" => ["finance", "equity", "grant", "expertise-and-advice"] } 
-        ]
-      )
+      business_support_api_has_schemes([{ 
+        "title" => "Graduate start-up", "business_support_identifier" => "graduate-start-up",
+        "stages" => ["grow-and-sustain"], "business_sizes" => ["up-to-249"], "sectors" => ["education"],
+        "support_types" => ["finance", "equity", "grant", "expertise-and-advice"] }],
+        { "postcode" => "WC2B 6SE", "support_types" => "finance,equity,grant,expertise-and-advice" })
+      business_support_api_has_schemes([{
+        "title" => "Manufacturing Services - Wales", "business_support_identifier" => "manufacturing-services-wales",
+        "stages" => ["grow-and-sustain"], "business_sizes" => ["under-10"], "sectors" => ["manufacturing"],
+        "support_types" => ["finance", "equity", "grant", "expertise-and-advice"] }],
+        { "postcode" => "SA99 1AR", "support_types" => "finance,equity,grant,expertise-and-advice" })
 
       visit "/#{APP_SLUG}/search"
-
-      BusinessSupportController.any_instance.should_not_receive(:search)
     end
 
     it "should filter results in the DOM" do
@@ -119,7 +128,6 @@ describe "Finding support options" do
 
       uncheck("loan")
       uncheck("recognition-award")
-      select "London", :from => "areas"
       select "10 - 249", :from => "business_sizes"
       select "Education", :from => "sectors"
       select "Grow and sustain", :from => "stages"
@@ -127,12 +135,10 @@ describe "Finding support options" do
       page.should have_selector('.filter-results-summary h3 span', text: '1')
       page.find('li.scheme h3').text.should == "Graduate start-up"
 
-      select "Scotland", :from => "areas"
       select "1000+", :from => "business_sizes"
 
       page.should have_selector('.filter-results-summary h3 span', text: '0')
 
-      select "Wales", :from => "areas"
       select "0 - 9", :from => "business_sizes"
       select "Manufacturing", :from => "sectors"
 
@@ -141,7 +147,6 @@ describe "Finding support options" do
 
       check "loan"
       check "recognition-award"
-      select "Any location", :from => "areas"
       select "Any number of employees", :from => "business_sizes"
       select "All", :from => "sectors"
       select "All", :from => "stages"
@@ -149,6 +154,24 @@ describe "Finding support options" do
       page.should have_selector('.filter-results-summary h3 span', text: '2')
       assert page.find('li.scheme', :text => "Graduate start-up")
       assert page.find('li.scheme', :text => "Manufacturing Services - Wales")
+    end
+
+    it "shouldn't filter when the postcode field is changed" do
+      page.should have_selector('.filter-results-summary h3 span', text: '2')
+
+      fill_in "Business postcode", :with => "WC2B 6SE"
+
+      page.should have_selector('.filter-results-summary h3 span', text: '2')
+    end
+
+    it "should filter by postcode on form submission" do
+      fill_in "Business postcode", :with => "WC2B 6SE"
+
+      # The submit button is hidden with js enabled, so mimick the return keypress.
+      page.execute_script("$('#document-filter').submit()")
+
+      assert page.find('.filter-results-summary h3 span', text: '1')
+      assert page.find('li.scheme', :text => "Graduate start-up")
     end
   end
 end
