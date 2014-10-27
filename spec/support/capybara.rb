bcb0e4b1f4f6cef0405e203b1f228d106dcced7e
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2014-10-27 14:43:31 +0000

    Fix broken PhantomJS tests, due to SSL
    phantomjs defaults to using SSLv3, which has now been disabled
    everywhere.  The protocol used can be changed with an option:
    
     --ssl-protocol=<val>                 Sets the SSL protocol
       (supported protocols: 'SSLv3' (default), 'SSLv2', 'TLSv1', 'any')
    
    This PR makes poltergeist and the  task set this option when invoking phantomjs.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,8 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, { phantomjs_options: ['--ssl-protocol=TLSv1'] })
+end
+
 Capybara.javascript_driver = :poltergeist
