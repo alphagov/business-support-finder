4880777f773c7cc9d0e72e6f4ce1491d67999365
Ben Janecke <benjanecke@gmail.com>
2015-05-29 16:14:29 +0100

    Remove unwanted sectors
    * information-communication-and-media
    * service-industries
    * travel-and-leisure

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -8,14 +8,11 @@ class Sector < OpenStruct
     "education" => "Education",
     "health" => "Health",
     "hospitality-and-catering" => "Hospitality and Catering",
-    "information-communication-and-media" => "Information, Communication and Media",
     "manufacturing" => "Manufacturing",
     "mining" => "Mining",
     "real-estate" => "Real Estate",
     "science-and-technology" => "Science and Technology",
-    "service-industries" => "Service Industries",
     "transport-and-distribution" => "Transport and Distribution",
-    "travel-and-leisure" => "Travel and Leisure",
     "utilities" => "Utilities",
     "wholesale-and-retail" => "Wholesale and Retail"
   }.map do |slug, name|
