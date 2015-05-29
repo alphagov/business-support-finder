54e41b1dab3e24dc9b0e68945a92fd9f2a6e3a26
Ben Janecke <benjanecke@gmail.com>
2015-05-29 16:13:25 +0100

    Fix formatting in HARDCODED_SECTORS hash

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -2,22 +2,22 @@ require 'ostruct'
 
 class Sector < OpenStruct
   HARDCODED_SECTORS = {
-    "agriculture"=>"Agriculture",
-    "business-and-finance"=>"Business and Finance",
-    "construction"=>"Construction",
-    "education"=>"Education",
-    "health"=>"Health",
-    "hospitality-and-catering"=>"Hospitality and Catering",
-    "information-communication-and-media"=>"Information, Communication and Media",
-    "manufacturing"=>"Manufacturing",
-    "mining"=>"Mining",
-    "real-estate"=>"Real Estate",
-    "science-and-technology"=>"Science and Technology",
-    "service-industries"=>"Service Industries",
-    "transport-and-distribution"=>"Transport and Distribution",
-    "travel-and-leisure"=>"Travel and Leisure",
-    "utilities"=>"Utilities",
-    "wholesale-and-retail"=>"Wholesale and Retail"
+    "agriculture" => "Agriculture",
+    "business-and-finance" => "Business and Finance",
+    "construction" => "Construction",
+    "education" => "Education",
+    "health" => "Health",
+    "hospitality-and-catering" => "Hospitality and Catering",
+    "information-communication-and-media" => "Information, Communication and Media",
+    "manufacturing" => "Manufacturing",
+    "mining" => "Mining",
+    "real-estate" => "Real Estate",
+    "science-and-technology" => "Science and Technology",
+    "service-industries" => "Service Industries",
+    "transport-and-distribution" => "Transport and Distribution",
+    "travel-and-leisure" => "Travel and Leisure",
+    "utilities" => "Utilities",
+    "wholesale-and-retail" => "Wholesale and Retail"
   }.map do |slug, name|
     new(:slug => slug, :name => name)
   end
