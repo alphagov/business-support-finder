3c3fa3a952be54f6afd1f245386291c594d50812
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-08-20 11:27:30 +0000

    Update secret_key setting, and disable session store.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BusinessSupportFinder::Application.config.secret_token = 'd12b85a61fa2806a9cfcaf0817a3ef6bde3894ea2e177bfbfe878c069d6a631124011775d37738ef9261e6b404046225c8d1767088b6343b5f03ba5ede738f93'
