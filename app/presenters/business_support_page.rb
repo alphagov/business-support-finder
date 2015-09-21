9eb64188fbdb7688a7942fe7f26b97818210fa01
tijmen <tijmen@gmail.com>
2015-09-21 09:03:57 +0100

    Add content_id to page data
    By including this here, the content_id will be automatically sent to
    panopticon. The content_id was generated by running `SecureRandom.uuid`
    in the rails console.

--- app/presenters/business_support_page.rb
+++ app/presenters/business_support_page.rb
@@ -1,6 +1,7 @@
 class BusinessSupportPage
   DATA = {
     slug: APP_SLUG,
+    content_id: "6f34c053-2f99-48a3-81e3-955fae00df69",
     title: "Finance and support for your business",
     description: "Find business finance, support, grants and loans backed by the government.",
     need_id: "100115",
