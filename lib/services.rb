b2061c4922ee006771431acdecfad516d8f18b64
Mo Baig <mo@mobaig.com>
2016-09-30 13:00:36 +0000

    Update gds-api-adapters to 36.4.0
    Update to the latest version, particularly as we've updated the
    Panopticon adapter to stop handling search-related attributes (see
    alphagov/gds-api-adapters#592).
    
    Includes a test helper update to remove deprecation warnings.

--- lib/services.rb
+++ lib/services.rb
@@ -10,6 +10,6 @@ module Services
   end
 
   def self.rummager
-    @rummager ||= GdsApi::Rummager.new(Plek.find("search"))
+    @rummager ||= GdsApi::Rummager.new(Plek.find("rummager"))
   end
 end
